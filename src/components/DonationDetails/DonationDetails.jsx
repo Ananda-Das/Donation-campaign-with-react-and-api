237f726fdd02c18ca5e32d0822197e6831b18765
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-09-25 23:18:21 +0600

    Update the Navbar

--- src/components/DonationDetails/DonationDetails.jsx
+++ src/components/DonationDetails/DonationDetails.jsx
@@ -9,7 +9,8 @@ const DonationDetails = () => {
       <div>
         <div className="w-[1000px] mx-auto">
           <div>
-            <img src={donationDetails.image} alt="" className="w-full" />
+            <img src={donationDetails.image} alt="" className="w-full mt-5" />
+            <button className="bg-[#FF444A] py-4 px-4 text-white rounded-md">Donate ${donationDetails.price}</button>
           </div>
           <h2 className="text-4xl font-semibold my-5">{donationDetails.title}</h2>
           <h2 className="text-justify">{donationDetails.description}</h2>
---
+++ src/components/Navbar/Logo.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+const Logo = () => {
+    return (
+        <div>
+            {/* <h1 className='text-xl font-semibold'>Phone Shop</h1> */}
+            <img src="../../../public/Logo.png" alt="" />
+        </div>
+    );
+};
+
+export default Logo;
